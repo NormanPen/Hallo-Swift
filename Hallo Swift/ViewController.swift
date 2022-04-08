ab46563d849bdc85bb32a2c3728d8bd1a7c514bf
Norman Pendzich <norman.pendzich@outlook.de>
2022-04-08 18:32:00 +0200

    Toggle Button erstellt

--- Hallo Swift/ViewController.swift
+++ Hallo Swift/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func switchButtonClicked(_ sender: UIButton) {
+        if myLabel.text == "Hello Swift" {
+            myLabel.text = "Hallo World"
+        } else {
+            myLabel.text = "Hello Swift"
+        }
+        
+        
+    }
+    
 }
 
